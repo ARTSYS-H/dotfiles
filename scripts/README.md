1172bba3f081c4520e26a446519b409fc45d2625
Lugh <lhflyextra300@gmail.com>
2025-06-20 15:54:00 +0200

    feat: add scripts
    Usefull scripts to manage my dotfiles or configuration related contents.

---
+++ scripts/README.md
@@ -0,0 +1,13 @@
+## Scripts
+
+Here, i put some usefull scripts to manage my dotfiles or configuration related contents.
+
+### Fonts Install
+
+[CodingFont](https://www.codingfont.com/)
+
+**Actual:** [Inconsolata Nerd Font](https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Inconsolata.zip), Slashed zero, takes inspiration from many different fonts and glyphs, subtle curves in lowercase.
+
+### Go Install
+
+Automatic Install and Update of direct `Go install` app, like lazygit or Go Linter...
---
+++ scripts/install-go-apps.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+if ! command -v "go" &>/dev/null; then
+  echo "Go isn't installed on the System." 1>&2
+  exit 1
+fi
+
+apps=(
+  "github.com/go-delve/delve/cmd/dlv@latest"
+  "github.com/nametake/golangci-lint-langserver@latest"
+  "github.com/golangci/golangci-lint/cmd/golangci-lint@latest"
+  "golang.org/x/tools/cmd/goimports@latest"
+  "golang.org/x/tools/gopls@latest"
+  "github.com/jesseduffield/lazygit@latest"
+  "github.com/charmbracelet/glow@latest"
+)
+
+function install_ask {
+  while true; do
+    echo "=== $1"
+    read -r -p "Install? [Y/n]: " answer
+    case $answer in
+      [Yy]* )
+      return 0
+      break
+      ;;
+      [Nn]* )
+      return 1
+      break
+      ;;
+      "" )
+      return 0
+      break
+      ;;
+    esac
+  done
+}
+
+function install {
+  go install "$1"
+}
+
+function install_apps {
+  for app in "${apps[@]}"; do
+    if install_ask "$app"; then
+      install "$app"
+    fi
+  done
+}
+
+if [[ "$1" != "--all" ]]; then
+  install_apps
+else
+  for app in "${apps[@]}"; do
+    install "$app"
+  done
+fi
